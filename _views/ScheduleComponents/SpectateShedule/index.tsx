0a91c32de465729ee0c6d64516b857d077281ca4
DADGET-ass <150382504+DADGET-ass@users.noreply.github.com>
2024-05-16 23:36:02 +0300

    corr

--- _views/ScheduleComponents/SpectateShedule/index.tsx
+++ _views/ScheduleComponents/SpectateShedule/index.tsx
@@ -1,11 +1,40 @@
+import { Arcticle } from "@/_views/ui/Arcticle";
+import { Calendar } from "@/_views/ui/Calendar";
+import { Title } from "@/_views/ui/Title/Index";
+import { IFacultets, getFacultets } from "@/data/api";
+import { useDateStore } from "@/data/store/useDateStore";
+import { useEffect, useState } from "react";
 
+import cls from './index.module.scss';
 
 const SpectateShedule = () => {
 
+    const { selectedDate } = useDateStore()
+    const [facultets, setFacultets] = useState<Array<IFacultets>>([]);
+
+    useEffect(() => {
+        getFacultets().then(e => {
+            setFacultets(e.facultets)
+        })
+    }, [])
 
     return (
-        <div></div>
-    )
-}
+        <>
+            <Arcticle>
+
+                <div className={cls.title}>
+                    <Title>Расписание</Title>
+                    <Calendar />
+                   
+                </div>
+
+                {/* {facultets.map((facult) => (
+                    <facult facult={facult} key={facult._id} />
+                ))} */}
+            </Arcticle>
+        </>
+    );
+};
 
 export {SpectateShedule}
+
